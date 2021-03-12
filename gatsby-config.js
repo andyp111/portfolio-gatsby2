440c38fc6c50ab2c23430eca920a0dedecc5a483
Andy Pham <andypham@Andys-MacBook-Pro.local>
2021-03-11 20:35:55 -0800

    added script so toggler would work

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  pathPrefix:'/andyp111.github.io',
+
   siteMetadata: {
     title: "andypham",
   },
@@ -23,6 +25,33 @@ module.exports = {
          storageKey: "darkMode",
          minify: true,
       },
-    }
+    },
+    {
+      resolve: 'gatsby-plugin-load-script',
+      options: {
+        src: 'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+        integrity:
+          'sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN',
+        crossorigin: 'anonymous',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-load-script',
+      options: {
+        src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
+        integrity:
+          'sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q',
+        crossorigin: 'anonymous',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-load-script',
+      options: {
+        src: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
+        integrity:
+          'sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl',
+        crossorigin: 'anonymous',
+      },
+    },
   ],
 };
