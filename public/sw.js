6c634004db565fc07b8820e5aa890a5278f17619
Andy Pham <andypham@Andys-MacBook-Pro.local>
2021-03-17 16:44:10 -0700

    downgraded babel runtime

--- public/sw.js
+++ public/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c17c720d2d092e0c7de9.js"
+    "url": "webpack-runtime-043c05ca5be13e710093.js"
   },
   {
     "url": "framework-ddff9c6a94258f6c8286.js"
@@ -39,14 +39,14 @@ self.__precacheManifest = [
     "url": "styles-719967e2ea75025a1d91.js"
   },
   {
-    "url": "app-88eb5e534aad3a253bc9.js"
+    "url": "app-cc18fafd551ab1e60cc7.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ff4b56aee6c8a4f5cd60603b0f7ac996"
+    "revision": "e69d47a3ba6a9c6196cbea1a36ef09cc"
   },
   {
-    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-a95b272f8c25b9a8a758.js"
+    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0a65868ff3ddd0892a08.js"
   },
   {
     "url": "polyfill-80a3750c0e0c3147bbe7.js"
@@ -141,7 +141,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-88eb5e534aad3a253bc9.js`))) {
+  if (!resources || !(await caches.match(`/app-cc18fafd551ab1e60cc7.js`))) {
     return await fetch(event.request)
   }
 
